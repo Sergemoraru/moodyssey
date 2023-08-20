fba1ee2187c6103e1fc604edcca2fb80c175e066
Serge Moraru <sergemoraru@gmail.com>
2023-08-20 16:16:20 -0400

    route to database. new entry is now functional

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,17 @@
+import { UserButton } from "@clerk/nextjs"
+
 const DashboardLayout = ({children}) => {
     return  <div className="h-screen w-screen relative">
         <aside className="absolute w-44 top-0 left-0 h-full border-r border-black/10">
             Mood
         </aside>
-        <div className="ml-44">
+        <div className="ml-44 h-full">
             <header className="h-16 border-b border-black/10">
-                Hello
+                <div className="h-full w-full px-6 flex items-center justify-end">
+                    <UserButton />
+                </div>
             </header>
-            <div>
+            <div className="h-full">
                 {children}
             </div>
         </div>
